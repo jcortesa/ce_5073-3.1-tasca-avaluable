a49159ca2965ed993b07ea50e21806c8cdad6c14
Jorge Cortés <jcortesanguita@gmail.com>
2024-01-05 02:44:59 +0100

    develop: Add Decision Tree model and prediction web service

--- ce_5073_3_1_tasca_avaluable/predict_app.py
+++ ce_5073_3_1_tasca_avaluable/predict_app.py
@@ -1,6 +1,8 @@
 import pickle
 from flask import Flask, jsonify, request
-from predict_service import predict_logistic_regression, predict_support_vector_machine
+from predict_service import (predict_logistic_regression, 
+                            predict_support_vector_machine, 
+                            predict_decision_tree)
 
 app = Flask('iris-predict')
 
@@ -15,6 +17,9 @@ with open('models/1-logistic-regression-model.pck', 'rb') as f:
 
 with open('models/2-support-vector-machine-model.pck', 'rb') as f:
     svm_sc, svm_model = pickle.load(f)
+
+with open('models/3-decision-tree-model.pck', 'rb') as f:
+    dt_model = pickle.load(f)
 
 @app.route('/logistic-regression/predict', methods=['POST'])
 def logistic_regression_predict():
@@ -44,6 +49,24 @@ def support_vector_machine_predict():
                                                                  petal_width,
                                                                  lr_sc,
                                                                  lr_model)
+    
+    result = {
+        'probable flower name': labels[class_label],
+        'probability of match': '{0:.5g}%'.format(class_probability * 100),
+    }
+
+    return jsonify(result)
+
+
+@app.route('/decision-tree/predict', methods=['POST'])
+def decision_tree_predict():
+    flower_data = request.get_json()
+    petal_length = float(flower_data['petal_length'])
+    petal_width = float(flower_data['petal_width'])
+
+    class_label, class_probability = predict_decision_tree(petal_length,
+                                                                 petal_width,
+                                                                 dt_model)
     
     result = {
         'probable flower name': labels[class_label],
--- ce_5073_3_1_tasca_avaluable/predict_service.py
+++ ce_5073_3_1_tasca_avaluable/predict_service.py
@@ -41,3 +41,24 @@ def predict_support_vector_machine(petal_length, petal_width, sc, model):
     probability_pred = model.predict_proba(X_std)[0]
 
     return y_pred, probability_pred[y_pred]
+
+def predict_decision_tree(petal_length, petal_width, model):
+    """Do predictions with Decision Tree model
+
+    Args:
+        petal_length (float): Length of petals in cm
+        petal_width (float): Width of petals in cm
+        model (sklearn.tree.DecisionTreeClassifier): Decision Tree model
+
+    Returns:
+        tuple containing
+
+        - y_pred (int64): probable flower index
+        - probability_pred (float): probability
+    """    
+    X = [[petal_length, petal_width]]
+    y_pred = model.predict(X)[0]
+
+    probability_pred = model.predict_proba(X)[0]
+
+    return y_pred, probability_pred[y_pred]
