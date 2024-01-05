dbfeb3a4a3bcfdc54f9bfbc939c7cd396633fe7f
Jorge Cortés <jcortesanguita@gmail.com>
2024-01-05 02:27:27 +0100

    develop: Add Support Vector Machine (SVM) model and prediction web service

--- ce_5073_3_1_tasca_avaluable/predict_app.py
+++ ce_5073_3_1_tasca_avaluable/predict_app.py
@@ -1,6 +1,6 @@
 import pickle
 from flask import Flask, jsonify, request
-from predict_service import predict_logistic_regression
+from predict_service import predict_logistic_regression, predict_support_vector_machine
 
 app = Flask('iris-predict')
 
@@ -13,6 +13,9 @@ labels = {
 with open('models/1-logistic-regression-model.pck', 'rb') as f:
     lr_sc, lr_model = pickle.load(f)
 
+with open('models/2-support-vector-machine-model.pck', 'rb') as f:
+    svm_sc, svm_model = pickle.load(f)
+
 @app.route('/logistic-regression/predict', methods=['POST'])
 def logistic_regression_predict():
     flower_data = request.get_json()
@@ -20,6 +23,24 @@ def logistic_regression_predict():
     petal_width = float(flower_data['petal_width'])
 
     class_label, class_probability = predict_logistic_regression(petal_length,
+                                                                 petal_width,
+                                                                 lr_sc,
+                                                                 lr_model)
+    
+    result = {
+        'probable flower name': labels[class_label],
+        'probability of match': '{0:.5g}%'.format(class_probability * 100),
+    }
+
+    return jsonify(result)
+
+@app.route('/support-vector-machine/predict', methods=['POST'])
+def support_vector_machine_predict():
+    flower_data = request.get_json()
+    petal_length = float(flower_data['petal_length'])
+    petal_width = float(flower_data['petal_width'])
+
+    class_label, class_probability = predict_support_vector_machine(petal_length,
                                                                  petal_width,
                                                                  lr_sc,
                                                                  lr_model)
--- ce_5073_3_1_tasca_avaluable/predict_service.py
+++ ce_5073_3_1_tasca_avaluable/predict_service.py
@@ -5,7 +5,29 @@ def predict_logistic_regression(petal_length, petal_width, sc, model):
         petal_length (float): Length of petals in cm
         petal_width (float): Width of petals in cm
         sc (sklearn.preprocessing.StandardScaler): Standardized data model
-        model (sklearn.linear_model import LogisticRegression): Linear Regression model
+        model (sklearn.linear_model.LogisticRegression): Linear Regression model
+
+    Returns:
+        tuple containing
+
+        - y_pred (int64): probable flower index
+        - probability_pred (float): probability
+    """    
+    X_std = sc.transform([[petal_length, petal_width]])
+    y_pred = model.predict(X_std)[0]
+
+    probability_pred = model.predict_proba(X_std)[0]
+
+    return y_pred, probability_pred[y_pred]
+
+def predict_support_vector_machine(petal_length, petal_width, sc, model):
+    """Do predictions with Support Vector Machine (SVM) model
+
+    Args:
+        petal_length (float): Length of petals in cm
+        petal_width (float): Width of petals in cm
+        sc (sklearn.preprocessing.StandardScaler): Standardized data model
+        model (sklearn.svm.SVC): Support Vector Machine model
 
     Returns:
         tuple containing
